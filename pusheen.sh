5b661704dbac9b57b9e36413be917a99b2e147f9
jd615645 <jd615645@users.noreply.github.com>
2016-10-24 21:01:17 +0800

    :cat2:pusheen shell

---
+++ pusheen.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo ""
+echo "                                       ▐▀▄       ▄▀▌   ▄▄▄▄▄▄▄               "
+echo "    + - -- - - - - - - - - - - - +     ▌  ▀▄▄▄▄▄▀  ▐▄▀▀ ██ ██ ▀▀▄            "
+echo "    |                            |    ▐    ▀ ▀ ▀                 ▀▄          "
+echo "    |    Hi! Sakamoto O ω O      |    ▌               ▄            ▀▄        "
+echo "    |                            |  ▀█   █▌  █  ▐█   ▀               ▌       "
+echo "    |  It's a good day to code!  |  ▀▌      ▀ ▀      ▀▀              ▐   ▄▄  "
+echo "    |                            |  ▐                                 ▌▄█ █  "
+echo "    + - -- - - - - - - - - - - - +  ▐                                 █ █▀   "
+echo "                                    ▐                                 █▀     "
+echo "                                    ▐                                 ▌      "
+echo "                                     ▌                               ▐       "
+echo "                                     ▐                               ▌       "
+echo "                                      ▌                             ▐        "
+echo "                                      ▐▄                           ▄▌        "
+echo "                                       ▀▄▄▀▀▀▀▀▄▄▀▀▀▀▀▀▀▄▄▀▀▀▀▀▄▄▀           "
+echo ""
+
+exit 0
